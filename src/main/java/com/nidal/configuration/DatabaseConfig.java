962ff010a541d09ef8e86874faf6ccf26832b91f
stephen-hoaxing <nidal.chalhoub@gmail.com>
2019-05-26 12:22:00 +0200

    properties - just a dev one

--- src/main/java/com/nidal/configuration/DatabaseConfig.java
+++ src/main/java/com/nidal/configuration/DatabaseConfig.java
@@ -2,10 +2,9 @@ package com.nidal.configuration;
 
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,9 +14,24 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 
 @Configuration
+@PropertySource("classpath:application.properties")
 @EnableNeo4jRepositories(basePackages = "com.nidal.repo")
 @EnableTransactionManagement
 public class DatabaseConfig extends Neo4jConfiguration {
+
+    @Value("${neo4j.username}")
+    private String userName;
+
+    @Value("${neo4j.password}")
+    private String password;
+
+    @Value("${neo4j.port}")
+    private String port;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfig () {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public SessionFactory getSessionFactory() {
@@ -26,11 +40,12 @@ public class DatabaseConfig extends Neo4jConfiguration {
 
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
+        String uri = String.format("http://%s:%s@localhost:%s", userName, password, port);
         org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration();
         configuration
                 .driverConfiguration()
                 .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
-                .setURI("http://neo4j:123456@localhost:7474");
+                .setURI(uri);
         return configuration;
     }
 
